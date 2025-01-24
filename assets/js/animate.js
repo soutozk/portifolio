b333a3178f4f2c457444eb23cc969d2b731e9286
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-01-24 18:56:45 -0300

    up

---
+++ assets/js/animate.js
@@ -0,0 +1,19 @@
+// Animação do contorno
+gsap.to("text", {
+  strokeDashoffset: 0,
+  duration: 3,
+  ease: "power2.inOut",
+  fill: "white",
+});
+
+setTimeout(function () {
+  document.querySelector(".start-animation").style.transition =
+    "opacity 1s ease-out";
+  document.querySelector(".start-animation").style.opacity = 0; // Esconde a animação inicial suavemente
+}, 2500); // Tempo igual à duração da animação do contorno
+
+setTimeout(function () {
+  document.querySelector(".start-animation").style.display = "none"; // Esconde a animação após a transição
+  document.querySelector(".container-home").style.display = "flex"; // Exibe a nova seção
+  document.querySelector(".container-home").style.opacity = 1; // Torna visível a nova seção
+}, 3000); // Aguarda a animação de contorno ser concluída
