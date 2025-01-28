40c6457ae9f9e59558671dd87f2c5aa28c4609a3
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-01-28 17:05:21 -0300

    adjust styles

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -10,10 +10,10 @@ setTimeout(function () {
   document.querySelector(".start-animation").style.transition =
     "opacity 1s ease-out";
   document.querySelector(".start-animation").style.opacity = 0; // Esconde a animação inicial suavemente
-}, 1500); // Tempo igual à duração da animação do contorno 2500
+}, 2500); // Tempo igual à duração da animação do contorno 2500
 
 setTimeout(function () {
   document.querySelector(".start-animation").style.display = "none"; // Esconde a animação após a transição
   document.querySelector(".container-home").style.display = "flex"; // Exibe a nova seção
   document.querySelector(".container-home").style.opacity = 1; // Torna visível a nova seção
-}, 1000); // Aguarda a animação de contorno ser concluída 3000
+}, 3000); // Aguarda a animação de contorno ser concluída 3000
